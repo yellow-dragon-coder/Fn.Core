82a3d024b47a84d55d500d4f7a63bf5d222037a8
Ahmet <yellow.draqon@gmail.com>
2019-12-29 17:57:08 +0300

    Add In extension method

---
+++ src/Fn.Core/CoreExtensions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fn.Core
+{
+    public static class CoreExtensions
+    {
+        public static bool In<T>(this T value, params T[] values) where T : IEquatable<T>
+        {
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+
+            if (values.Length == 0)
+                return false;
+
+            foreach (var v in values)
+            {
+                if (value.Equals(v))
+                    return true;
+            }
+
+            return false;
+        }
+
+        public static bool In<T>(this T value, IEnumerable<T> values) where T : IEquatable<T>
+        {
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+
+            if (!values.Any())
+                return false;
+
+            foreach (var v in values)
+            {
+                if (value.Equals(v))
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ tests/Fn.Core.Tests/CoreTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Fn.Core.Tests
+{
+    public class CoreTests
+    {
+        [Fact]
+        public void In_Should_Work()
+        {
+            "A".In("A", "B", "C").Should().BeTrue();
+            2.In(1, 2, 3, 4).Should().BeTrue();
+            5.In(1, 2, 3, 4).Should().BeFalse();
+            1.In(new List<int> { 1, 2, 3 }).Should().BeTrue();
+            const string s = null;
+            Assert.Throws<NullReferenceException>(() => s.In("X", "Y", "Z"));
+        }
+    }
+}
